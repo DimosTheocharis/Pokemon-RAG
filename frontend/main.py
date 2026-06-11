f86c7385ef18eccc5eced95af7b274d0b741c95a
Dimos Theocharis <dimostheocharis10@gmail.com>
2026-06-11 17:44:01 +0300

    Set ups frontend to use llama index rag

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,9 @@
 import streamlit as st
-from utils import responseGenerator
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings 
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+import time, os, json
 
 st.title("Pokemon RAG")
 
@@ -13,7 +17,49 @@ for message in st.session_state.messages:
     chatMessage = st.chat_message(message["role"])
     chatMessage.markdown(message["content"])
 
-print('eo')
+
+def file_metadata_func(file_path: str):
+    pokemonName = ('r' + file_path).split('\\')[-1][:-4]
+    if os.path.isfile(f'../backend/metadata/{pokemonName}.json'):
+        with open(f'../backend/metadata/{pokemonName}.json', 'r') as f:
+            metadata = json.load(f)
+            return metadata
+    else:
+        print(f'This is not a file: {f'../backend/metadata/{pokemonName}.json'}')
+
+    return {}
+
+@st.cache_resource(show_spinner=True)
+def loadData():
+    documents = SimpleDirectoryReader('../backend/data', file_metadata=file_metadata_func).load_data()
+    
+    # Set up the LLM and embedding model
+    Settings.llm = Ollama(
+        model="llama3.1:8b",
+        request_timeout=60,
+        context_window=8000,
+        system_prompt = """
+            You are a Pokemon assistant.
+            You MUST answer ONLY using the provided documents.
+            Do NOT say you don't know if the answer exists in the documents.
+            If the user misspells a Pokémon name, map it to the closest Pokémon in the provided documents.
+            If the correct Pokémon exists in the documents, always answer using it.
+            You can include metadata when needed.
+        """
+    )
+
+    Settings.embed_model = OllamaEmbedding(
+        model_name="bge-m3"
+    )
+
+    index = VectorStoreIndex.from_documents(documents)
+    return index
+
+
+index = loadData()
+queryEngine = index.as_query_engine(similarity_top_k=10)
+
+
 
 
 prompt = st.chat_input("Say something")
@@ -28,6 +74,9 @@ if (prompt):
 
     # Display assistant response in chat message container
     chatMessage = st.chat_message("assistant")
-    response = chatMessage.write_stream(responseGenerator())
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    response = queryEngine.query(prompt)
+    if (response):
+        chatMessage.markdown(response.response)
+
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response.response})
